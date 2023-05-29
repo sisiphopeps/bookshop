a7719adfc65e5ffddfdb98c1fb75f0ca7e82f454
unknown <sisiphopepengweni@gmail.com>
2023-05-29 16:39:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+let shop = [
+    {
+        image: "https://postimg.cc/QFYPYcCT",
+        desc: "it ends with us",
+        price: "R129.95",
+    },
+
+    {
+        image: "https://postimg.cc/Pvk3B41S",
+        desc: "it starts with us",
+        price: "R145,00",
+    },
+
+    {
+        image: "https://postimg.cc/YhvRbTct",
+        desc: "reminders of him",
+        price: "xxx",
+    },
+
+    {
+        image: "https://postimg.cc/2bJb6c0x",
+        desc: "losing hope",
+        price: "xxx",
+    },
+
+    {
+        image: "https://postimg.cc/mP5GKvkV",
+        desc: "regretting you",
+        price: "xxx",
+    },
+
+    {
+        image: "https://postimg.cc/1fTQdxfB",
+        desc: "november 9",
+        price: "xxx",
+    },
+
+    {
+        image: "",
+        desc: "",
+        price: "",
+    },
+
+    {
+        image: "",
+        desc: "",
+        price: "",
+    },
+
+    {
+        image: "",
+        desc: "",
+        price: "",
+    }
+]
+
